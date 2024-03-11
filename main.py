d7fa51466047403e5fe89173620c0bdd653bbcd4
otacoinkor <down80kg@gmail.com>
2024-03-11 22:14:37 +0900

    update

--- main.py
+++ main.py
@@ -2,9 +2,9 @@
 import streamlit as st
 import paho.mqtt.client as mqtt
 
-mqtt_broker = "your_mqtt_broker_address"
+mqtt_broker = "broker.hivemq.com"
 mqtt_port = 1883  # Default MQTT port if not using SSL/TLS
-mqtt_topic = "your_topic"
+mqtt_topic = "otaco_sys"
 
 # 스트림릿 앱의 UI를 구성합니다.
 st.title('메시지 전송 웹앱')
