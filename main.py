e67b283c8cc3b9532b22fd3ce590a7f1fffe8eb8
otacoinkor <down80kg@gmail.com>
2024-03-12 16:44:31 +0900

    update

--- main.py
+++ main.py
@@ -7,14 +7,15 @@ mqtt_port = 1883  # Default MQTT port if not using SSL/TLS
 mqtt_topic = "otaco_sys"
 
 # 스트림릿 앱의 UI를 구성합니다.
-st.title('메시지 전송 웹앱')
+st.title('세차장 MQTT 테스트')
 
 client = mqtt.Client()
 client.connect(mqtt_broker, mqtt_port, 60)
 
 if st.button('전송'):
-    message = "streamlit MQTT test message"
+    message = "streamlit MQTT 테스트"
     client.publish(mqtt_topic, message)
+
     st.success('Message sent successfully!')
 
 
